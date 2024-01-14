4d80dd3208499dadc82b048a9040e87bc60f0ca8
siraj <ksiraj728@gmail.com>
2024-01-14 19:10:56 +0530

    added Accordian to Course page

---
+++ src/components/AccordianCourse.js
@@ -0,0 +1,24 @@
+import { useState } from "react";
+
+const Accordian = ({ Question, Answer }) => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <div className=" ml-3 mb-5">
+        <div className="flex justify-between px-4">
+          <h1 className="text-xl text-gray-500">{Question}</h1>
+          <p onClick={() => setShow(!show)}>
+            {" "}
+            {show ? (
+              <i className="fa-solid fa-chevron-down"></i>
+            ) : (
+              <i className="fa-solid fa-chevron-up"></i>
+            )}{" "}
+          </p>
+        </div>
+        {show && <h1 className="text-lg px-4">{Answer}</h1>}
+      </div>
+    </>
+  );
+};
+export default Accordian;
